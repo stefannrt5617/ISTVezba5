18442f216c35a2d6553087b6c1b5d16370b65b3d
stefannrt5617 <fansteblea98@hotmail.com>
2020-04-12 16:52:48 +0200

    Dodat modul BookInfo

---
+++ BookInfo/index.js
@@ -0,0 +1,34 @@
+
+const knjige = [
+    {
+        id: 1,
+        naziv: "Berlin",
+        autor: "Dzejms Paterson"
+    },
+
+    {
+        id: 2,
+        naziv: "Saptac",
+        autor: "Donato Karizi"
+    },
+
+    {
+        id: 3,
+        naziv: "Ubisto na terenu za golf",
+        autor: "Agata Kristi"
+    },
+
+    {
+        id: 4,
+        naziv: "Serlok Holmes",
+        autor: "Artur Konan Dojl"
+    },
+
+    {
+        id: 5,
+        naziv: "Igra Prestola",
+        autor: "Dzordz RR Martin"
+    }
+];
+
+module.exports = knjige
